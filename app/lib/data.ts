c81e4c474887bec44db5e21229316b5c6f9df2fe
Carlos Valdivieso <fvaldiviesohalo@gmail.com>
2024-07-11 18:02:24 -0600

    Sign In
    sign in and logout authentication added.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,13 +13,11 @@ import { formatCurrency } from './utils';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 1500));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
